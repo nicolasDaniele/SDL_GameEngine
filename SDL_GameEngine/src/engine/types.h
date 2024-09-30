a693d3d95eb20821af3318b592aa12573130cea1
Nicolas Daniele <nicolasdaniele@ravegan.com>
2024-09-29 23:45:23 -0300

    Created types and util headers

---
+++ SDL_GameEngine/src/engine/types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <inttypes.h>
+
+typedef uint8_t ui8;
+typedef uint16_t ui16;
+typedef uint32_t ui32;
+typedef uint64_t ui64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef float f32;
+typedef double f64;
+typedef size_t usize;
