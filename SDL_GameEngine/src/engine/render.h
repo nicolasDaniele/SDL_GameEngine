77d7bb9b18001e2f8701fdcb904587f405179982
NicolasDan <68474932+nicolasDaniele@users.noreply.github.com>
2024-10-08 20:13:56 -0300

    Reapply "Added batch rendering logic"
    This reverts commit 5b2108c69fc7ef42e7e9a402217efb8acd2ba8e1.

--- SDL_GameEngine/src/engine/render.h
+++ SDL_GameEngine/src/engine/render.h
@@ -4,6 +4,16 @@
 #include <linmath.h>
 #include "types.h"
 
+typedef struct batch_vertex {
+	vec2 position;
+	vec2 uvs;
+	vec4 color;
+} Batch_Vertex;
+
+#define MAX_BATCH_QUADS 10000
+#define MAX_BATCH_VERTICES 40000
+#define MAX_BATCH_ELEMENTS 60000
+
 SDL_Window *render_init(void);
 void render_begin(void);
 void render_end(SDL_Window *window);
@@ -12,3 +22,4 @@ void render_quad_line(vec2 pos, vec2 size, vec4 color);
 void render_line_segment(vec2 start, vec2 end, vec4 color);
 void render_aabb(f32 *aabb, vec4 color);
 f32 render_get_scale();
+void append_quad(vec2 postion, vec2 size, vec4 texture_coordinates, vec4 color);
--- SDL_GameEngine/src/engine/render/render_internal.h
+++ SDL_GameEngine/src/engine/render/render_internal.h
@@ -7,7 +7,8 @@
 
 SDL_Window *render_init_window(ui32 width, ui32 height);
 void render_init_color_texture(ui32 *texture);
-void render_init_shaders(ui32 *shader_default, f32 render_width, f32 render_height);
+void render_init_shaders(ui32 *shader_default, ui32 *shader_batch, f32 render_width, f32 render_height);
+void render_init_batch_quads(ui32 *vao, ui32 *vbo, ui32 *ebo);
 void render_init_quad(ui32 *vao, ui32 *bvo, ui32 *ebo);
 void render_init_line(ui32 *vao, ui32 *vbo);
 ui32 render_shader_create(const char *path_vert, const char *path_frag);
